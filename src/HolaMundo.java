50f8ef9b3eff72138cc0e3befe72075d8f22bea7
Paul Hidalgo <paulesh5@gmail.com>
2023-07-04 09:49:01 -0500

    Arreglada la vaina

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -1,2 +1,44 @@
-package PACKAGE_NAME;public class HolaMundo {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class HolaMundo {
+    private JPanel rootPanel;
+    private JButton Calcular;
+    private JLabel Titulo;
+    private JTextField Nom;
+    private JTextField peso;
+    private JLabel Nombre;
+    private JLabel Peso;
+    private JLabel estatura;
+    private JTextField Estatura;
+    private JLabel IMC;
+    private JLabel resultado;
+
+    public HolaMundo() {
+        Calcular.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                /*double peso_ingresado = Double.parseDouble(peso.getText());
+                double estatura_ingresada = Double.parseDouble(estatura.getText());
+                double imc = peso_ingresado / (estatura_ingresada*estatura_ingresada);
+                resultado.setText(" "+imc);*/
+                double imc;
+                double peso_ingresado;
+                double estatura_ingresada;
+                peso_ingresado = Double.parseDouble(peso.getText());
+                estatura_ingresada = Double.parseDouble(Estatura.getText());
+                imc = (peso_ingresado/(estatura_ingresada*estatura_ingresada));
+                resultado.setText(Double.toString(peso_ingresado+estatura_ingresada));
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("IMC");
+        frame.setContentPane(new HolaMundo().rootPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
